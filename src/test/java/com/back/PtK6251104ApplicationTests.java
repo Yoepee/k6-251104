8e0ef72b4950b236a5fe6d272c987715d0ea2328
Yoepee <kdy2975@naver.com>
2025-11-04 12:23:04 +0900

    초기설정

---
+++ src/test/java/com/back/PtK6251104ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtK6251104ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
